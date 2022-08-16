5a58373adf28110d4f1361c413ddcdb9c0be0548
Lucas Renan <lucasrenandns96@gmail.com>
2022-08-16 13:51:54 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 
 const container = document.querySelector(".container")
-const btn = document.querySelector(".btn")
+const btn = container.querySelector(".btn")
 
 btn.onclick = function () {
     this.classList.toggle("active")
